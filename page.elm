07f83e203819b13e83a29f983df1f4888c70d670
Christine Koppelt <cko@users.noreply.github.com>
2016-09-25 18:26:33 +0200

    fetch markdown from file

--- page.elm
+++ page.elm
@@ -1,39 +1,78 @@
-import Html.App exposing (beginnerProgram)
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Markdown
+import Html.App
+import Http
+import Task exposing (Task)
 
-main = beginnerProgram { model = "", view = view, update = update }
 
-type Msg = NewContent String
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (always Sub.none)
+        }
 
-update (NewContent content) oldContent = 
-  content
 
+type Msg
+    = Fetch
+    | FetchSuccess String
+    | FetchError Http.Error
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "", fetchCmd )
+
+
+view : Model -> Html Msg
 view model =
-  div
-    [ class "container" ]
-    [
-      header, 
-      content
-    ]
+    div [ class "container" ]
+        [ header
+        , Markdown.toHtml [] model
+        ]
 
 
 header : Html a
 header = 
   div 
     [ class "navigation" ] 
-    [text "navigation"]
+    [ text "navigation"]
 
 
-content : Html a
-content =
-  Markdown.toHtml [] markdown
+url : String
+url =
+    "http://localhost:8000/test.md"
 
-  
-markdown : String
-markdown = """
 
-test
+fetchTask : Task Http.Error String
+fetchTask =
+    Http.getString url
 
-"""
+
+fetchCmd : Cmd Msg
+fetchCmd =
+    Task.perform FetchError FetchSuccess fetchTask
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Fetch ->
+            ( model, fetchCmd )
+
+        FetchSuccess name ->
+            ( name, Cmd.none )
+
+        FetchError error ->
+            ( toString error, Cmd.none )
+
